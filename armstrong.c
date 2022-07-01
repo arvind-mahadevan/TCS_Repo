0598d3c87863e2843ec7b80edd6e43e82c2a8031
arvind-mahadevan <45776768+arvind-mahadevan@users.noreply.github.com>
2022-07-01 15:28:13 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+//Armstrong number
+int Arm(int n)
+{
+  int newnum=n;
+  int newnum1=n;
+  int m=0;
+  while(newnum1!=0)
+  {
+    newnum1/=10;
+    m++;
+  }
+  int sum=0;
+  while(newnum!=0)
+  {
+    sum+=pow(newnum%10,m);
+    newnum/=10;
+  }
+  if(sum==n)
+    return 1;
+  return 0;
+}
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  if(Arm(n)==1)
+  {
+    printf("YES");
+  }
+  else
+    printf("NO");
+}
